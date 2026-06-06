cf34f65bdbc8696e27dd49baf8481b89b202d04b
JAYADITHYA PRANEETH MACHAVARAPU <machavarapujayadithyapraneeth@gmail.com>
2026-06-07 02:09:01 +0530

    Refactor TransactionTaskPOJO1 class structure

--- TransactionTaskPOJO1.java
+++ TransactionTaskPOJO1.java
@@ -9,24 +9,11 @@ public class TransactionTaskPOJO1 {
 	private long Amount;
 	private long TimeStamp;
 	
-//	public TransactionTaskPOJO1(long fromAccountNumber, long toAccountNumber, String password, long amount, long transactionId) {
-//		System.out.println("TransactionTaskPOJO constructor called-successfully created a transaction task object");
-//		this.TransactionId = transactionId;
-//		this.ToAccountNumber = toAccountNumber;//this refers to the current class variable or current object attribute
-//		//TransactionTaskPOJO.ToAccountNumber = toAccountNumber;// this refers to a class variable or an attribute of a class's bluprint only 
-//		this.FromAccountNumber = fromAccountNumber;
-//		this.Password = password;
-//		this.Amount = amount;
-//		this.TimeStamp = System.currentTimeMillis();
-//	}
-	
 	public void TransactionTaskPOJO1() {
 		
 	}
 		
-	
 	public void settermethod(long fromAccountNumber, long toAccountNumber, String password, long amount, long transactionId) {
-		//System.out.println("TransactionTaskPOJO setter called-successfully created and assigned values to a transaction task object");
 		this.TransactionId = transactionId;
 		this.ToAccountNumber = toAccountNumber;//this refers to the current class variable or current object attribute 
 		this.FromAccountNumber = fromAccountNumber;
@@ -49,9 +36,6 @@ public class TransactionTaskPOJO1 {
 	public long getAmount() {
 		return Amount;
 	}
-//	public long getTimeStamp() {
-//		return TimeStamp;
-//	}
 	public long getTransactionID() {
 		return TransactionId;
 	}
